fa13f48825deadff6268d9f85345f2be9e8396a0
Moses <mochengo29@gmail.com>
2023-08-30 15:13:11 +0300

    Completed assignment

--- lib/migrations/migrations/env.py
+++ lib/migrations/migrations/env.py
@@ -2,8 +2,6 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from models import Base
-target_metadata = Base.metadata
 
 from alembic import context
 
@@ -20,8 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models import Base
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
